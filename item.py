3c630f44c77e19e59608e2d50e83442fe017fb99
leoAudureau <leo.audureau@edu.esiee.fr>
2024-12-09 17:48:26 +0000

    ajout de la classe item

---
+++ item.py
@@ -0,0 +1,9 @@
+class Item:
+    def __init__(self, name,description,weigh):
+        self.name = name
+        self.description = description
+        self.weight = weight
+    
+    def __str__:
+        return f"{self.name} : {self.description} ({self.weight} kg)"
+
