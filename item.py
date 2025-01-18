adfeccfe06c1d67b66c153f3c41259bc6495aeab
leoAudureau <leo.audureau@edu.esiee.fr>
2025-01-18 10:18:36 +0000

    pylint item

--- item.py
+++ item.py
@@ -1,9 +1,26 @@
-class Item:
-    def __init__(self, name,description,weigh):
+"""
+This module defines the Item class used in a text-based adventure game.
+"""
+
+class Item:  # pylint: disable=too-few-public-methods
+    """Represents an item in a text-based adventure game."""
+
+    def __init__(self, name, description, weight):
+        """
+        Initialize an Item instance.
+
+        :param name: str, the name of the item
+        :param description: str, a brief description of the item
+        :param weight: float, the weight of the item
+        """
         self.name = name
         self.description = description
-        self.weight = weigh
-   
-    def __str__(self):
-        return f"{self.name} : {self.description} ({self.weight} kg)"
+        self.weight = weight
 
+    def __str__(self):
+        """
+        Return a string representation of the item.
+
+        :return: str, formatted string describing the item
+        """
+        return f"{self.name} : {self.description} ({self.weight} kg)"
